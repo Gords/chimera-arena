2f606e7bd2aafa863a9d4a44c1164292e1a40ee9
Gords <gordsftrl@gmail.com>
2026-02-21 16:19:51 +0900

    wip

--- server/index.ts
+++ server/index.ts
@@ -33,7 +33,7 @@ if (IS_PRODUCTION) {
   app.use(express.static(clientDist));
 
   // SPA fallback
-  app.get('*', (_req, res) => {
+  app.get('{*path}', (_req, res) => {
     res.sendFile(path.join(clientDist, 'index.html'));
   });
 }
